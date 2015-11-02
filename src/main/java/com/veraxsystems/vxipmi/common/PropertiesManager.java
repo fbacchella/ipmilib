abaec8adab7e4c8f78c376c8b4afab9970a9cc57
Fabrice Bacchella <fbacchella@spamcop.net>
2015-11-02 15:49:14 +0100

    Improved PropertiesManager, added a singloton holder, some static added a info log changed to debug log

--- src/main/java/com/veraxsystems/vxipmi/common/PropertiesManager.java
+++ src/main/java/com/veraxsystems/vxipmi/common/PropertiesManager.java
@@ -9,24 +9,21 @@ import org.apache.log4j.Logger;
 
 public class PropertiesManager {
 
-    private static PropertiesManager instance;
+    private static final Logger logger = Logger.getLogger(PropertiesManager.class);
 
-    private Map<String, String> properties;
+    private static class PropertiesManagerHolder {		
+		private final static PropertiesManager instance = new PropertiesManager();
+	}
 
-    private Logger logger = Logger.getLogger(PropertiesManager.class);
+    private final Map<String, String> properties = new HashMap<String, String>();
 
     private PropertiesManager() {
-        properties = new HashMap<String, String>();
-
         loadProperties("/connection.properties");
         loadProperties("/vxipmi.properties");
     }
 
     public static PropertiesManager getInstance() {
-        if (instance == null) {
-            instance = new PropertiesManager();
-        }
-        return instance;
+        return PropertiesManagerHolder.instance;
     }
 
     private void loadProperties(String name) {
@@ -35,16 +32,17 @@ public class PropertiesManager {
             properties.load(getClass().getResourceAsStream(name));
 
             for (Object key : properties.keySet()) {
-                this.properties.put(key.toString(), properties.getProperty(key.toString()));
+                properties.put(key.toString(), properties.getProperty(key.toString()));
             }
-
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
     }
 
     public String getProperty(String key) {
-        logger.info("Getting " + key + ": " + properties.get(key));
+    	if(logger.isDebugEnabled()) {
+            logger.debug("Getting " + key + ": " + properties.get(key));    		
+    	}
         return properties.get(key);
     }
 
