338637281de6b796be6489df03049bbc5a163ebd
Fabrice Bacchella <fbacchella@spamcop.net>
2015-11-24 23:33:54 +0100

    A small break to escape from an active waiting loop

--- src/main/java/com/veraxsystems/vxipmi/connection/Connection.java
+++ src/main/java/com/veraxsystems/vxipmi/connection/Connection.java
@@ -278,6 +278,7 @@ public class Connection extends TimerTask implements MachineObserver {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				logger.error(e.getMessage(), e);
+				break;
 			}
 			++time;
 		}
